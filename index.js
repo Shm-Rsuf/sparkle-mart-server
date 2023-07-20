11203da4ae80188765527bee0719403a669c028f
shm_usuf <shmusuf@gmail.com>
2023-07-20 12:19:34 +0600

    server setup basic done

--- index.js
+++ index.js
@@ -3,21 +3,35 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+/* VARIABLES */
 const port = process.env.PORT || 4000;
+const uri = process.env.MONGO_URI;
 
+/* EXPRESS APP */
 const app = express();
 
+/* MIDDLEWARES */
 app.use(express.json());
 app.use(
   cors({
     credentials: true,
   })
 );
-
+/* TEST APIs */
 app.get("/", (req, res) => {
   res.status(200).json({ message: "welcome to sparkle mart server" });
 });
 
-app.listen(port, () => {
-  console.log(`Our server is running of port : ${port}`);
-});
+/* DATA BASE (DB) */
+mongoose
+  .connect(uri, {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server is running on port : ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
